b417844632cd51e14057a1a6d3f2f8f66966a11a
Chris <chris@threecomma.io>
2022-03-21 10:26:56 -0400

    fixes

--- preflight.sh
+++ preflight.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Preflight"
 
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sh
 brew install git
 brew install homebrew/cask-versions/1password-beta --cask
